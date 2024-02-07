c66a085c81aad7cb61edecd7e71be9ae85622489
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-02-07 09:02:31 +0000

    func2

--- 03_basics/1_function.js
+++ 03_basics/1_function.js
@@ -48,7 +48,8 @@ console.log("Result: ", result); //op=>Result:  8
 
 //----------------------------------------------------------------------
 
-function loginUserMessage(username = "sam"){
+
+/*function loginUserMessage(username = "sam"){
     if(!username){
         console.log("PLease enter a username");
         return
@@ -56,6 +57,56 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 console.log(loginUserMessage("ayushi"))
+
+*/
+
+
+//------------------------------------------------------------------------------
+
+
+/*function calculateCartPrice(val1, val2, ...num1) //...  is rest or sprad operting
+{
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))// op=> [ 500, 2000 ]
+*/
+
+//----------------------------------------------------------------------------------------------------
+
+/*const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+  //or
+handleObject({
+    username: "sam",
+    price: 399
+})
+*/
+
+
+//-------------------------------------------------------
+
+/*const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000])); //op=>400
+
+*/
+
+
+
 
 
 
