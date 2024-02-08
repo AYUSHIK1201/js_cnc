0f281662cf631f311ccfaab927379d5032d4f8d8
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-02-08 07:41:37 +0000

    iife

---
+++ 03_basics/4_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE) ---to avoid global scope varibles pollution
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})(); // 1st () is function and 2nd () is execution or call remember to add ; at end to dd new iife function
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
