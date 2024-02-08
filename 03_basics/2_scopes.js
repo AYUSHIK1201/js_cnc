a39be3c94df8656474b514f88755e6f6a17f5bb6
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-02-08 07:35:28 +0000

    arrow

---
+++ 03_basics/2_scopes.js
@@ -0,0 +1,83 @@
+/*if (true) {
+    let a = 10
+    const b = 20
+    var c = 300
+   
+    
+}
+//console.log(a); //op=> a is not defined
+//console.log(b);  //op=> b is not defined
+//console.log(c); //op=>300 ---thats why we do not use var because it has scope issue and give result even after being in a scope
+
+//var c = 300
+
+*/
+
+
+
+/*
+var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+console.log(a);//op=>300
+ //console.log(b); //op=>b is not defined
+ console.log(c); //op=>300
+
+ */
+
+ //-----------------------------------------------------------------------------------------------------------
+
+ //child function can acess parent function variable but not vice versa
+
+ /*
+ function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+     //console.log(website);//op=>website is not defined--as its scope is onlu inside two function
+
+     two()// op=>hitesh
+
+}
+
+one()
+
+*/
+
+/*
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+         console.log(username + website);// op=>hitesh youtube
+    }
+    //console.log(website);//op=>website is not defined
+}
+
+//console.log(username);// op=>username is not defined
+
+*/
+
+
+/*console.log(addone(5))//can initialize before declare
+
+function addone(num){
+    return num + 1
+}*/
+
+
+
+/*addTwo(5)//op=> Cannot access 'addTwo' before initialization--cant inditazie before declare
+const addTwo = function(num){
+    return num + 2
+}
+*/
---
+++ 03_basics/3_arrow.js
@@ -0,0 +1,57 @@
+/*const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() //can declare like this 
+     {
+        console.log(`${this.username} , welcome to website`);//here current context or this is user --op=>hitesh , welcome to website
+       console.log(this); //give complete obj
+    }
+}
+
+ //user.welcomeMessage()
+// user.username = "sam"
+//user.welcomeMessage()//op=>sam , welcome to website
+
+//console.log(this);//op=>{}
+
+*/
+
+ /*function chai(){
+     let username = "hitesh"
+     console.log(this.username);//op=>undefined--can only use "this" inside object not function
+ }
+
+ chai()*/
+//--------------------------------------------------------------------------------------------------------------------
+ /*const chai = function () 
+ {
+    let username = "hitesh"
+     console.log(this.username);
+
+    }
+    */
+
+    /*const chai =  () => {
+        let username = "hitesh"
+        console.log(this);//op=>{}
+    }
+    chai()
+    */
+
+    const addTwo = (num1, num2) => 
+    {
+             return num1 + num2
+         }
+//can write in same line witht rturn keyword s it contain only 1 line statement calld implecit 
+         //const addTwo = (num1, num2) =>  num1 + num2 
+            //or
+        //const addTwo = (num1, num2) => ( num1 + num2 )
+        const addTwo = (num1, num2) => ({username: "hitesh"})
+         
+         
+        // console.log(addTwo(3, 4))//op=>7
+
+       /* // const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()*/
