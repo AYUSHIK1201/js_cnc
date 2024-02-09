1f51f6f3c6ae41a29fc7457b576271f882ff10cb
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-02-09 17:24:52 +0000

    filter

---
+++ 05_iteration/5-for-each.js
@@ -0,0 +1,60 @@
+/* sentax-----------------------------
+Array_name.forEach( function_name (val){
+    console.log(val);
+} )
+*/
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+/*
+coding.forEach( function (val){
+    console.log(val);//p=>js ruby java .......
+} )
+*/
+
+
+/*
+//arrow function
+coding.forEach( (item) => {
+  console.log(item);
+ } )
+ */
+
+/*
+
+
+//by calling refernce of function
+function printMe(item){
+  console.log(item);
+}
+
+coding.forEach(printMe)
+
+*/
+/*
+coding.forEach( (item, index, arr)=> {
+console.log(item, index, arr);
+} )
+*/
+
+/*
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )*/
