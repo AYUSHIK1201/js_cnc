125691f915483272a612751b0da6713889e11e36
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-02-10 15:42:34 +0000

    map

---
+++ 05_iteration/8.js
@@ -0,0 +1,50 @@
+//reducer runs like a loop 
+/*const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce(function (accumulator, currval) 
+{
+  console.log(`acc: ${accumulator} and currval: ${currval}`);
+  
+  return accumulator + currval // after this accumulator new value become accumulator + currval
+}, 0) //0 is  initial value of accumulator and currval  is current value
+*/
+
+/*op=> /*op=>acc: 0 and currval: 1
+acc: 1 and currval: 2
+acc: 3 and currval: 3*/
+
+//or--------------------------------------------------
+
+/*
+//by arrow function
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+*/
+
+/*--------------------------------------------------------------------------------------------------------------
+//in objects
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
+*/
