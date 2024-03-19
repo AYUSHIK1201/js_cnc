fa89a6b122b42dffd1423be964fb40b0798e5624
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-03-19 09:06:45 +0000

    inheritance

---
+++ 10_class&oops/7_inheritance.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)//refer constructor(username) and set value rhis.username behind the scene to use it here
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
