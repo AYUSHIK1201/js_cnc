cfecc9c41e4bfa00b32339c186675ddf313b0f4a
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-10-02 05:52:57 +0000

    date

--- 01_basics/6_number_and_maths.js
+++ 01_basics/6_number_and_maths.js
@@ -1,18 +1,18 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100) //obj of number type
 // console.log(balance);//op=> [Number:100]
-
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));//op=>100.0//start after decimal
+// console.log(balance.toString().length);//op=>3
+//console.log(balance.toFixed(1));//op=>100.0//start after decimal
 
 const otherNumber = 123.8966
-
-// console.log(otherNumber.toPrecision(4)); //op=>123.9 //start frm the 1st digit if 3 then convert to.exponential => 
+// console.log(otherNumber.toPrecision(4)); //op=>123.9 //start from the 1st digit 
+//console.log(otherNumber.toPrecision(3));//op=>124
+//console.log(otherNumber.toPrecision(2));//op=>1.2e+2//if 2 then convert to exponential 
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));//put commas in number
+//console.log(hundreds.toLocaleString('en-IN'));//op=>10,00,000//put commas in number
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
@@ -24,11 +24,13 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());//op=> give random.value between 0 and 1
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);//floor remove decimals
+//console.log(Math.random());//op=> give random.value between 0 and 1
+//console.log((Math.random()*10) + 1);
+//console.log(Math.floor(Math.random()*10) + 1);//floor remove decimals
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min) //give random vlaue betweeen max nd min
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min) //give random vluee betweeen max nd min
+
+// +++++++++++++ other +++++++++++++++++++++++++++++
--- 01_basics/7_dates.js
+++ 01_basics/7_dates.js
@@ -1,13 +1,13 @@
 // Dates
 
-let myDate = new Date()//creating new vriable fo date
-console.log(myDate.toString());//=Wed Jan 24 2024 17:05:57 GMT+0000 (Coordinated Universal Time)
- console.log(myDate.toDateString());//=Wed Jan 24 2024
- console.log(myDate.toLocaleString());//=1/24/2024, 5:05:57 PM
-console.log(typeof myDate);//=object
+let myDate = new Date()//creating new vriable fo date type obj
+//console.log(myDate.toString());//=Wed Jan 24 2024 17:05:57 GMT+0000 (Coordinated Universal Time)
+//console.log(myDate.toDateString());//=Wed Jan 24 2024
+//console.log(myDate.toLocaleString());//=1/24/2024, 5:05:57 PM
+//console.log(typeof myDate);//=object
 
 
-// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23)//mounth starts from 0 in JS 
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
@@ -15,16 +15,16 @@ let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
-
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());//give value in milli sec
+ 
+//console.log(myTimeStamp);//op=>1727848175038
+// console.log(myCreatedDate.getTime());//op=>1673654400000 //give value in milli sec
 // console.log(Math.floor(Date.now()/1000));//conrt mili sec to sec
 
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+//console.log(newDate);//op=>2024-10-02T05:51:17.971Z
+//console.log(newDate.getMonth() + 1);//op=>10 //month
+//console.log(newDate.getDay());//op=>3//ie wednesday
 
 newDate.toLocaleString('default', {
     weekday: "long",
