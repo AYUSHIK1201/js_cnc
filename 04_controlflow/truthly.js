fcc6aba207f15c13595c4065fbd9f0fc0235d85c
AYUSHIK1201 <78696284+AYUSHIK1201@users.noreply.github.com>
2024-10-04 08:16:06 +0000

    loops

--- 04_controlflow/truthly.js
+++ 04_controlflow/truthly.js
@@ -3,7 +3,7 @@
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 //truthy values
-// "0", 'false', " ", [], {}, function(){}
+// "0", 'false', " ", [], {}, function(9){}
 
 
 /*
--- 05_iteration/1_for.js
+++ 05_iteration/1_for.js
@@ -51,7 +51,7 @@ for (let index = 1; index <= 20; index++)
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log(`Detected 5`);
-        continue
+        continue //forgive 1 time anfcontinue executing code 
     }
    console.log(`Value of i is ${index}`);
     
